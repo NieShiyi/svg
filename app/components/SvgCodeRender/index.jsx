60a604daf9184cc04c383174d31a06c9e5db6eda
Nie Shiyi <shiyi.nie@qitantech.com>
2025-07-09 14:06:13 +0800

    feat: create SvgCodeRender 把code自动转成带格式的字符串显示 把图形显示和查看代码放在同一个组件中

---
+++ app/components/SvgCodeRender/index.jsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+import { renderToStaticMarkup } from "react-dom/server";
+import * as prettier from "prettier/standalone";
+import * as parserHtml from "prettier/parser-html";
+import CodeRender from "../CodeRender";
+import "./index.css";
+
+const SvgCodeRender = ({ children, sheets, layout = "flex" }) => {
+  const [codeStr, setCodeStr] = useState();
+
+  useEffect(() => {
+    const run = async () => {
+      try {
+        const rawCode = renderToStaticMarkup(children);
+        const formattedCode = await prettier.format(rawCode, {
+          parser: "html",
+          plugins: [parserHtml],
+        });
+        setCodeStr(formattedCode);
+      } catch (e) {
+        console.error("Prettier format error", e);
+      }
+    };
+    run();
+  }, []);
+
+  return (
+    <div className={layout}>
+      <div className="code">{children}</div>
+      <div className="codeStr">
+        {codeStr ? <CodeRender code={codeStr} /> : null}
+      </div>
+      <ul className="sheets">
+        {sheets?.map((item) => {
+          return (
+            <div className="sheet">
+              <CodeRender code={item} />
+            </div>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default SvgCodeRender;
